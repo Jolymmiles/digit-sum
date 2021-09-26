826aefaab7ddbee614fa7c38a9992a8c4c74651c
Валентин <valmak04@gmail.com>
2021-09-26 15:28:48 +0400

    Done

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,6 +3,9 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        // write your code here
+        Scanner sc = new Scanner(System.in);
+        String a = sc.nextLine();
+        //System.out.print(a.substring(1, 2));
+        System.out.print(  Integer.parseInt(a.substring(0, 1).trim()) + Integer.parseInt(a.substring(1, 2).trim()) + Integer.parseInt(a.substring(2, 3).trim()));
     }
 }
