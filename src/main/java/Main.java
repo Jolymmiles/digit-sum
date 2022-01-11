77f80170c068af5d62f6c1130bcdc7d613bbc431
Valentine <valmak29@gmail.com>
2022-01-11 17:15:59 +0400

    Done

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,7 +5,8 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String a = sc.nextLine();
-        //System.out.print(a.substring(1, 2));
+        String digit1 = a.substring(0, 1), digit2 = a.substring(1, 2), digit3 = a.substring(2, 3);
         System.out.print(  Integer.parseInt(a.substring(0, 1).trim()) + Integer.parseInt(a.substring(1, 2).trim()) + Integer.parseInt(a.substring(2, 3).trim()));
+
     }
 }
